6e3da36e03dc98b86a9bb0bc32e89c60823862bf
Thainainan03 <toypoyomn03@outlook.com>
2021-08-30 17:10:00 +0700

    test

---
+++ demo/go.mod
@@ -0,0 +1,5 @@
+module demo
+
+go 1.16
+
+require github.com/thanainan03/golinenotify v0.0.0-20210830090333-046193f0341a // indirect
--- demo/main.go
+++ demo/main.go
@@ -1,4 +1,4 @@
-package golinenotify_test
+package main
 
 import (
 	"fmt"
@@ -7,18 +7,18 @@ import (
 	"github.com/thanainan03/golinenotify"
 )
 
-func Example() {
+func main() {
 	url := golinenotify.GetAuthorizeUrl("S36gTcAhUTXAHFBQ2G23t1", "http://localhost:3333/line-notify/callback", "444")
 	fmt.Println(url)
 
 	a, err := golinenotify.GetAccessToken("S36gTcAhUTXAHFBQ2G23t1", "TLW3cBjdohAHuYLw3tnCL188IX1xTsSHUNE3aZalY3x", "http://localhost:3333/line-notify/callback", "tqKQNNfIT2XkZWpJfSae4m")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 	fmt.Println(a)
 
 	err2 := golinenotify.Send("bCjP5KfzRF48sdpOkzx9WKyfkDtMR05vG4Z5Oor5NeH", "test")
 	if err != nil {
-		log.Fatal(err2)
+		log.Println(err2)
 	}
 }
